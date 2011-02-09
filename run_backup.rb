c94bf10d2f9b697eaf735c98574db74eac1884d8
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-02-09 18:00:45 +0100

    rename yaml files to .yml

--- run_backup.rb
+++ run_backup.rb
@@ -4,8 +4,8 @@ require 'rubygems'
 require File.join(File.dirname(__FILE__), 'lib', 'backup.rb')
 
 Lockfile.new 'state.lock', :retries => 2 do
-  config_file = GmailBackup::YAMLFile.new('config')
-  state_file = GmailBackup::YAMLFile.new('state')
+  config_file = GmailBackup::YAMLFile.new('config.yml')
+  state_file = GmailBackup::YAMLFile.new('state.yml')
 
   GmailBackup::IMAPBackup.new(config_file, state_file).run
 end
