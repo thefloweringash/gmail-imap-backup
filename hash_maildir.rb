03963e3d08fdf9331879daddcc781fc8d01aa429
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-09-29 16:18:11 +0200

    iterate dir with .each

--- hash_maildir.rb
+++ hash_maildir.rb
@@ -8,23 +8,30 @@ def hash_dir(path)
   maildir = Maildir.new(path, false)
 
   [:cur, :new, :tmp].each do |mailstat|
+    $stderr.puts "#{maildir.path}: #{mailstat}"
+    counter = 0
     files = maildir.list(mailstat)
     files.each do |file|
       parseme = file.data
       parseme = parseme[0, [parseme.length, 10000].min ]
       hash = EmailHash.hashme(parseme) 
       puts "#{file.path}\t#{hash}"
+      if (counter=counter+1) % 100 == 0
+        $stderr.puts "#{counter} / #{files.count}"
+      end
     end
   end
 
-  search_path = File.join(path, '*')
-  keys = Dir.glob(search_path, File::FNM_DOTMATCH)
-  keys.each do |key|
-#    puts "key: #{key}"
-    next unless File.directory? key
-    next if File.basename(key) == '.'
-    next if File.basename(key) == '..'
-    hash_dir(key)
+  Dir.open(path) do |dir|
+    dir.each do |key|
+      #$stderr.puts "key: #{key}"
+      next if key == '.' or key == '..'
+
+      dirpath = File.join(path, key)
+      next unless File.directory? dirpath
+
+      hash_dir(dirpath)
+    end
   end
 end
 
