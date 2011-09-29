0cfc3e7c214c7099b12f377e9f3e4ac10b5ea71b
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-09-29 17:14:36 +0200

    add or remove hash files in order, then print result

---
+++ merge_hash_lists.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require File.join(File.dirname(__FILE__), 'lib', 'backup.rb')
+
+if ARGV.count == 0 or ARGV.count%2 != 0
+  puts "Always -a[dd] <file> or -r[em] <file>"
+else
+  num_files = ARGV.count/2
+  hash2path = {}
+  (0..num_files-1).each do |fileindex|
+    op = ARGV[fileindex*2 + 0]
+    file = ARGV[fileindex*2 + 1]
+    puts "op: #{op} file: #{file}"
+
+    add = (op == '-a' or op == '-add')
+    File.open(file) do |infile|
+      while (line = infile.gets)
+        parts = line.split(/\t/,2)
+        if add
+          hash2path[parts[1]] = parts[0]
+        else
+          hash2path.delete(parts[1])
+        end 
+      end
+    end
+  end
+
+  hash2path.each do |hash, path|
+    puts "#{path}\t#{hash}"
+  end
+end
