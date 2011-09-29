895b9216fc79b9f6d96c8203f19fac9d728f5475
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-09-29 17:29:09 +0200

    tool to convert hash list into tsv table with path \t from \t subject

---
+++ hash_list_to_tsv.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require File.join(File.dirname(__FILE__), 'lib', 'backup.rb')
+
+if ARGV.count != 1
+  puts "Specify precisely one filename"
+else
+  File.open(ARGV[0]) do |infile|
+    while (line = infile.gets)
+      parts = line.split(/\t/,2)
+      data = File.open(parts[0]) {|f| f.read}
+      mail = Mail.new(data)
+      add = ""
+      if mail["X-Uniform-Type-Identifier"] == "com.apple.mail-draft"
+        add = "DRAFT>"
+      end
+      puts "#{parts[0]}\t#{mail.from}\t#{add}#{mail.subject}"
+    end
+  end
+end
